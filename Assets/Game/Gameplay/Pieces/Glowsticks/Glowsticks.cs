40c3ca44f5603f16786a1777df2340b297d9665a
Lucas Soares <lucassoares1793@gmail.com>
2024-02-16 01:37:54 -0300

    (WIP) Tweaking animated outline -- checkpoint

--- Assets/Game/Gameplay/Pieces/Glowsticks/Glowsticks.cs
+++ Assets/Game/Gameplay/Pieces/Glowsticks/Glowsticks.cs
@@ -7,15 +7,22 @@ public class Glowsticks : MonoBehaviour {
 
     [SerializeField, Range(0f, 3f)] float intensity;
     [SerializeField, Range(0f, 1f)] float thickness;
+    [SerializeField, Range(0f, 10f)] float detailX;
+    [SerializeField, Range(0f, 10f)] float detailY;
 
     static readonly int intensityID = Shader.PropertyToID("_Intensity");
     static readonly int thicknessID = Shader.PropertyToID("_Thickness");
+    static readonly int detailLevelID = Shader.PropertyToID("_DetailLevel");
 
-    void Awake() {
+    void OnEnable() {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void OnValidate(){
+
+        // This is editor only so dont really care about performance here
+        // Actually this entire script is mostly for editor editing the object
+        spriteRenderer = GetComponent<SpriteRenderer>();
         if(spriteRenderer == null){
             return;
         }
@@ -29,6 +36,7 @@ public class Glowsticks : MonoBehaviour {
         propertyBlock.SetFloat(intensityID, intensity);
         // Just scale down thickness a bit to make it easier to edit in editor
         propertyBlock.SetFloat(thicknessID, thickness/10f);
+        propertyBlock.SetVector(thicknessID, new Vector4(detailX, detailY, 0f, 0f));
         spriteRenderer.SetPropertyBlock(propertyBlock);
     }
 }
